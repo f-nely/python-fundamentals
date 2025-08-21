95797b18efcd63df68487962ebb55d2aa22129ad
nely de faria <nelly.nf54@gmail.com>
2025-08-21 12:36:37 -0300

    Create challenge-1.py

---
+++ src/challenge-1.py
@@ -0,0 +1,6 @@
+"""
+Complete o código no ponto indicado, de forma que ele exiba no console as mensagens "Olá Mundo!" e "Bem-vindo a formação Python da Treinaweb!".
+"""
+#Informe aqui
+print("Olá Mundo!")
+print("Bem-vindo a formação Python da Treinaweb!")
