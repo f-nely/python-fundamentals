af44541c82705377c3722485be83f0cf6e766ddc
nely de faria <nelly.nf54@gmail.com>
2025-08-21 12:36:47 -0300

    Create challenge-2.py

---
+++ src/challenge-2.py
@@ -0,0 +1,8 @@
+# Leia dois dados do terminal, uma string e um valor inteiro e os exiba no console
+
+#Informe o seu código aqui
+curso = input()
+num = int(input())
+print(curso)
+print(num)
+#multiple-assignment 
