a58bed1d18f8737324907aa563a6652a7b719a81
nely de faria <nelly.nf54@gmail.com>
2025-08-21 12:37:01 -0300

    Create challenge-3.py

---
+++ src/challenge-3.py
@@ -0,0 +1,21 @@
+"""
+Sabendo que os dados obtidos pela função input(), podem ser convertidos para array com a função split(), os atribuam a três variáveis e em seguida, exiba os valores no terminal
+"""
+
+# sentence = "Python Fundamentos Treinaweb"
+sentence = input("Digite uma frase: ")
+words = sentence.split()
+# print(type(words))
+# print(words[0])
+str1 = words[0]
+str2 = words[1]
+str3 = words[2]
+print(words[0])
+print(words[1])
+print(words[2])
+
+""" linguagem, nivel, companhia = input().split()
+
+print(linguagem)
+print(nivel)
+print(companhia) """
