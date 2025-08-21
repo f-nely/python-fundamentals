400155ffe855b242604b510fffa4f9d6b1e88721
nely de faria <nelly.nf54@gmail.com>
2025-08-21 12:34:40 -0300

    Create variables.py

---
+++ src/variables.py
@@ -0,0 +1,11 @@
+name = "Rayan"
+age = 22
+lastName = "Cherki"
+
+print(name, age, lastName)
+print("Hi,", name, lastName, "your age is ", age)
+print(f"Hi, {name} {lastName}, your age is ", age) # f string
+
+
+
+
