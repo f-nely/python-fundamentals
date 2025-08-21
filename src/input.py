1e952b41fced80a47d70d79c3ffcbccbf0c2ed90
nely de faria <nelly.nf54@gmail.com>
2025-08-21 12:35:21 -0300

    Create input.py

---
+++ src/input.py
@@ -0,0 +1,7 @@
+name = input("Type your name: ")
+lastName = input("Type your last name: ")
+age = int(input("Type your age: "))
+
+print(type(age))
+
+print(f"Hello, {name} {lastName}, your age is {age}!")
