6991d28bffb814f09c509111d219b74bd2b1ff5e
nely de faria <nelly.nf54@gmail.com>
2025-08-22 13:57:59 -0300

    Create arithmetic-operators.py

---
+++ src/arithmetic-operators.py
@@ -0,0 +1,24 @@
+# Addition Operator
+num1 = 2
+num2 = 3
+print(f"The sum between {num1} and {num2} is {num1 + num2}")
+
+# Subtraction Operator
+print(f"The subtraction between {num1} and {num2} is {num1 - num2}")
+
+# Multiplication Operator
+print(f"The multiplication between {num1} and {num2} is {num1 * num2}")
+
+# Division Operator 
+# Float division
+print(f"The division between {num1} and {num2} is {round(num1 / num2, 2)}")
+
+# Integer division( Floor division)
+print(f"The integer division between {num1} and {num2} is {num1 // num2}")
+
+# Modulus Operator
+print(f"The modulus between {num1} and {num2} is {num1 % num2}")
+
+# Exponentiation Operator
+print(f"The exponentiation between {num1} and {num2} is {num1 ** num2}")
+
