2971038a2a52c7d2328b78a500fef3694c70f4fc
nely de faria <nelly.nf54@gmail.com>
2025-08-22 14:02:11 -0300

    Create challenge-5.py

---
+++ src/challenge-5.py
@@ -0,0 +1,13 @@
+"""
+Dado um valor inteiro N, complete o código abaixo de acordo com as condições:
+Se N for par, exiba no console: "Par";
+Se N for ímpar, exiba no console: "Ímpar".
+"""
+
+n = int(input("Enter a number: "))
+
+if (n % 2 == 0):
+  print("Par")
+else:
+  print("Ímpar")
+
