e1929a7c6e0c489924f40c4b4b0cbc70869e6704
nely de faria <nelly.nf54@gmail.com>
2025-08-22 14:01:56 -0300

    Create challenge-4.py

---
+++ src/challenge-4.py
@@ -0,0 +1,14 @@
+"""
+Leia dois valores inteiros da entrada padrão e exiba em três linhas os resultados:
+
+1. A primeira linha contém a soma dos dois valores;
+2. A segunda linha contém a diferença entre os dois valores;
+3. A terceira linha contém o produto dos dois valores.
+"""
+
+value1 = int(input("Enter a number: "))
+value2 = int(input("Enter another number: "))
+
+print(value1 + value2)
+print(value1 - value2)
+print(value1 * value2)
