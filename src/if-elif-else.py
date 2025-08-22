2363e0bc85a51d77b3bef1e3fb3f81c26a92879f
nely de faria <nelly.nf54@gmail.com>
2025-08-22 13:58:42 -0300

    Create if-elif-else.py

---
+++ src/if-elif-else.py
@@ -0,0 +1,16 @@
+soccerPlayer, age = "Reijnders", 27
+
+if age >= 18:
+  print(f"{soccerPlayer} is of legal age!")
+else:
+  print(f"{soccerPlayer} is a minor!")
+
+
+soccerPlayer2, age2 = "Trafford", 22
+if age2 <= 17:
+  print(f"{soccerPlayer2} is teenager!")
+elif age2 >= 18 and age2 <= 50:
+  print(f"{soccerPlayer2} is adult!")
+else:
+  print(f"{soccerPlayer2} is elderly!")
+
