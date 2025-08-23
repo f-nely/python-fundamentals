fc8d8ce6e4683a5fb97cb34ef72f7161d9887916
nely de faria <nelly.nf54@gmail.com>
2025-08-23 11:41:21 -0300

    Create list-example.py

---
+++ src/list-example.py
@@ -0,0 +1,21 @@
+""" lista = ["Arroz", "Feijão"]
+lista.append("Macarrão")
+print(lista) # Arroz, Feijão, Macarrão """
+
+""" lista = ["Arroz", "Feijão"]
+lista.insert(1, "Macarrão")
+print(lista) # Arroz, Macarrão, Feijão """
+
+lista = ["Arroz", "Feijão"]
+lista.insert(1, "Macarrão")
+print(lista) # Arroz, Macarrão, Feijão
+lista.remove("Macarrão")
+print(lista) # Arroz, Feijão
+
+"""
+pop(índice): remove o elemento da lista;
+index(elemento): retorna o índice do elemento na lista;
+count(elemento): retorna a quantidade de vezes em que um elemento aparece na lista;
+sort(): ordena a lista;
+reverse(): inverte a ordem da lista.
+"""
