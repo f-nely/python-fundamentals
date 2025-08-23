652589eeb6e0db54fe364987647391c5e92c49e8
nely de faria <nelly.nf54@gmail.com>
2025-08-23 11:40:51 -0300

    Create challenge-9.py

---
+++ src/challenge-9.py
@@ -0,0 +1,8 @@
+"""
+Defina uma classe chamada PrimeiraClasse() e que contenha os atributos curso e descricao (nesta ordem). O método construtor deve receber dois parâmetros, um para cada atributo
+"""
+
+class PrimeiraClasse:
+  def __init__(self, curso, descricao):
+    self.curso = curso
+    self.descricao = descricao
